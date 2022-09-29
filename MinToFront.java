7b772a6ff1bbabbe97db10bfb61aaf0e7c16813a
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-29 13:40:23 +0530

    Update MinToFront.java

--- MinToFront.java
+++ MinToFront.java
@@ -1,3 +1,10 @@
+/*
+	Write a method minToFront that takes an ArrayList of integers as a parameter and that moves the 
+	minimum value in the list to the front, otherwise preserving the order of the elements. 
+**	For example, if a list stores the following values: {3, 8, 92, 4, 2, 17, 9} and you make 
+	this call: minToFront(list); it should store the following values after the call: {2, 3, 8, 92, 4, 17, 9} 
+	You may assume that the list stores at least one value
+*/
 package com.testexercise.bll;
 
 import java.util.ArrayList;
@@ -13,7 +20,7 @@ public class MinToFront {
 		
 		int min = minToFront(list);//call minToFront method
 		
-		list.remove(new Integer(2));//remove 2
+		list.remove(new Integer(min));//remove minimum number
 		
 		list.add(0,min);//add min number at first position
 		
