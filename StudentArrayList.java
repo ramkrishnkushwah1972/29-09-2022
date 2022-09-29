874e80c23405da0cb46ef0f2cea2197413fc42f4
Ramkrishna Kushwah <105698577+ramkrishnkushwah1972@users.noreply.github.com>
2022-09-29 17:36:30 +0530

    Update StudentArrayList.java

--- StudentArrayList.java
+++ StudentArrayList.java
@@ -1,3 +1,46 @@
+/*	2.	Create an ArrayList object students, and store the following students in that list.
+
+		Studen
+			studentcode
+			studentname
+			age
+			state
+
+Student Code	Student Name	Age	State
+AF0216223	Aryan Raj	21	Andra Pradhesh
+AF0216224	Bivor Kumar	22	Andra Pradhesh
+AF0216227	SUSHMITA KUMARI	23	Madhya Pradesh
+AF0216231	Ragiv Zafar	24	Maharashtra
+AF0216232	RAHUL MAHTO	25	Orissa
+AF0216234	Nainsi Kumari	19	Gujarath
+AF0216236	MD ALI	26	Madhya Pradesh
+AF0216238	ABHISHEK KUMAR	22	Andra Pradhesh
+AF0216240	AFROZ ANSARI	21	Maharashtra
+AF0216259	RITIK RAJ	20	Orissa
+AF0216263	Anant Kumar	25	Andra Pradhesh
+AF0216305	BABU KUMAR	24	Gujarath
+AF0216307	NIKITA KUMARI	23	Madhya Pradesh
+AF0216353	PRANAV PANDEY	22	Orissa
+AF0216964	Radheshyam Kumar	21	Maharashtra
+AF0216973	Ankur Utpal	19	Gujarath
+AF0217607	Banty Mishra	18	Madhya Pradesh
+AF0217615	RIYA KUMARI	20	Karnataka
+AF0217791	Priyadarshani Kumari	21	Maharashtra
+AF0223373	Chanchal Thakur	22	Orissa
+AF0221549	SATENDRA KUMAR	23	Maharashtra
+AF0216212	Pappi Verma	24	
+AF0216244	Ramkrishna Kushwah	25	Andra Pradhesh
+AF0216245	Yogita Tamoliya	26	Orissa
+
+Find the students aged over 20
+
+Find students from the state Andhra Pradhesh
+
+Sort students by their age.
+
+	Sort students by state and student name.
+*/
+
 package com.testexercise.bll;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -66,13 +109,11 @@ public class StudentArrayList {
 				System.out.println(s);
 		}
 
-		//Sort students by their age.
-		Collections.sort(students);  
-		
 		System.out.println("**************************************************************"
 						+"\n**************************************************************");
+		//Sort students by their age.
 		System.out.println("Sorted students by age ascending order : ");
-		
+		Collections.sort(students);
 		//Traversing list through Iterator
 		Iterator<Student> itr=students.iterator();
 		while(itr.hasNext()){  
@@ -81,18 +122,8 @@ public class StudentArrayList {
 		
 		System.out.println("**************************************************************"
 				+"\n**************************************************************");
-		//Sort students by student name.
-		System.out.println("Sort students by Name...");  
-		Collections.sort(students,new StateAndNameComparator());
-		
-		Iterator<Student> itr1 =students.iterator();
-		while(itr1.hasNext())
-			System.out.println(itr1.next());
-		
-		System.out.println("**************************************************************"
-				+"\n**************************************************************");
-		//Sort students by student State.
-		System.out.println("Sort students by State...");  
+		//Sort students by student State and Name.
+		System.out.println("Sort students by State and Name...");  
 		Collections.sort(students,new StateAndNameComparator());
 		
 		Iterator<Student> itr2 =students.iterator();
